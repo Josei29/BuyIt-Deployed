018d7c8bf56bbc5ee640a3632c5573afe5a12a0d
Josei29 <jjimenez.r98@gmail.com>
2019-04-01 17:16:02 -0500

    Static Files

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use('/static', express.static(path.join(__dirname, './client/build')))
+  app.use('/static', express.static('public'));
 }
 
 // Creating the connection to our DB
