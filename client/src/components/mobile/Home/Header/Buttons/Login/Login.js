56bd0dfc34acb25fb538131399c6e719225f88e2
Josei29 <jjimenez.r98@gmail.com>
2019-04-02 18:23:04 -0500

    Assets Folder

--- client/src/components/mobile/Home/Header/Buttons/Login/Login.js
+++ client/src/components/mobile/Home/Header/Buttons/Login/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import "./Login.css";
 import LoginSVG from "./LoginSVG"; 
 
 const login = (props) => {
--- client/src/components/mobile/Home/Header/Buttons/Signup/Signup.js
+++ client/src/components/mobile/Home/Header/Buttons/Signup/Signup.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import "./Signup.css";
 import SignupSVG from "./SignupSVG";
 
 const signup = (props) => {
